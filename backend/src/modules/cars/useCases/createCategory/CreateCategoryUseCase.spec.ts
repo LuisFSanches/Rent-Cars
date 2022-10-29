d339cc8a1e7f7d73a69153dfce3af1d7fe917c7c
LuisF <luis-lipe@hotmail.com>
2022-10-29 15:54:08 -0300

    correção sintaxe.

--- backend/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ backend/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -8,7 +8,7 @@ let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 describe("Create Category", () => {
   beforeEach(() => {
     categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
-    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
   })
   it("Should be able to create a new category", async () => {
     const category = {
